60eff411c46043754ede0154ea19baa0356c26e8
juan.a.mata.guzzo <juan.a.mata.guzzo@avanade.com>
2025-06-11 17:07:56 +0200

    ssr working

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-page',
@@ -7,4 +8,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './about-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AboutPageComponent { }
+export default class AboutPageComponent implements OnInit {
+  private _pageTitle = 'About page';
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle(this._pageTitle);
+    this.meta.updateTag({name: 'description', content: 'This is the about page.'});
+    this.meta.updateTag({name: 'og:title', content: this._pageTitle});
+    this.meta.updateTag({name: 'keywords', content: 'Pokemon,Pikachu,Bulbasaur'});
+  }
+}
--- src/app/pages/pricing-page/pricing-page.component.ts
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pricing-page',
@@ -7,4 +8,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent { }
+export default class PricingPageComponent implements OnInit {
+  private _pageTitle = 'Pricing page';
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle(this._pageTitle);
+    this.meta.updateTag({name: 'description', content: 'This is the pricing page.'});
+    this.meta.updateTag({name: 'og:title', content: this._pageTitle});
+    this.meta.updateTag({name: 'keywords', content: 'Pokemon,Pikachu,Bulbasaur'});
+  }
+}
